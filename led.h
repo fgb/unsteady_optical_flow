57fe6250b47536c75cec8ea6ba38b1dea36c38fe
Fernando L. Garcia Bermudez <fgb@eecs.berkeley.edu>
2012-04-30 14:01:56 -0700

    Reverting to previous led.h definitions.

--- led.h
+++ led.h
@@ -4,16 +4,16 @@
 * Date: 2010-07-25
 * Author: stanbaek
 ******************************************************************************/
+
 #ifndef __LED_H
 #define __LED_H
 
+
 #include "utils.h"
 
-#define LED_RED			(LED_1)
-#define LED_GREEN		(LED_2)
-#define LED_BLUE		(LED_3)
-#define LED_ORANGE      (LED_3)
-//#define LED_WHITE		(_LATE1)
+#define LED_RED     LED_1
+#define LED_GREEN   LED_2
+#define LED_ORANGE  LED_3
+
 
 #endif // __LED_H
-
