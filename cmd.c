e06b0574e8d390d6eb29caeae9c371a3ef861bd7
Fernando L. Garcia Bermudez <fgb@eecs.berkeley.edu>
2012-05-16 13:19:32 -0700

    Adding header.

--- cmd.c
+++ cmd.c
@@ -1,9 +1,43 @@
 /*
- * Receiving and transmitting queue handler
+ * Copyright (c) 2010-2012, Regents of the University of California
+ * All rights reserved.
  *
- * created on 2011-3-8 by stanbaek, fgb
- * modified by hhu for optical flow project
- * for use with hhu-lib code base
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the University of California, Berkeley nor the names
+ *   of its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * Command queue handlers
+ *
+ * by Stanley S. Baek
+ *
+ * v.beta
+ *
+ * Revisions:
+ *  Stanley S. Baek                 2010-7-10   Initial release
+ *  w/Fernando L. Garcia Bermudez   2011-3-8    Adaptation to fgb codebase
+ *  Humphrey Hu                     2012        Adaptation to latest radio.
+ *
  */
 
 //#include "init_default.h"
